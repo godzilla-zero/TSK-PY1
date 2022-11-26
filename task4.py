89314a2cd1c3fa85e2b4b00bfb8e08659e803ccd
godzilla-zero <115462633+godzilla-zero@users.noreply.github.com>
2022-11-26 23:52:16 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,15 +1,15 @@
 import json
 
-INPUT_FILE = "input.csv"  # во входном файле по умолчанию не те данные!
+INPUT_FILE = "input.csv"  
 
 
 def csv_to_list_dict(filename, delimiter=",", new_line="\n") -> list[dict]:
     with open(filename, "r") as f:  # TODO реализовать конвертер из csv в json
         splitted_list = [line.replace(new_line, '').split(delimiter) for line in f]
         row_numbers = len(splitted_list)
-        headers_index = 0  # пусть заголовки были в первой строке csv файла
+        headers_index = 0 
         list_dict = []
-        for i in range(headers_index+1, row_numbers):  # после строки с заголовками идут строки со значениями
+        for i in range(headers_index+1, row_numbers): 
             list_dict.append(dict(zip(splitted_list[0], splitted_list[i])))
         return list_dict
 
